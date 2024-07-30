ca4fbe178e9a70b1d0e091cd46563f1d62b6431e
minko-des <lijigongze@gmail.com>
2024-07-31 00:49:04 +0900

    TOPpage Markup 240731

--- js/main.js
+++ js/main.js
@@ -14,22 +14,43 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 */
 
+/* サイドバー/背景の表示/非表示 */
 $(document).ready(function() {
-    const $menuBtn = $(".c-menu-btn");  //menuボタン
+    const $menuBtn = $(".menu-btn");  //menuボタン
     const $sidebar = $(".p-sidebar");  //サイドバー
     const $closeBtn = $(".c-close-btn");  //×ボタン
-    const $sidebarBg = $(".p-sidebar__bg")  //背景色
+    const $sidebarBg = $(".p-sidebar__bg");  //背景色
 
-    //menuボタンクリックでサイドバー表示
+    //menuボタンクリックイベント
     $menuBtn.on("click", function() {
-        $sidebar.addClass("u-open");
-        $sidebarBg.addClass("u-overlay");
+        $sidebar.addClass("u-open");  //サイドバースライドイン
+        $sidebarBg.addClass("u-overlay"); //背景色透明度0.5
     });
 
-    //×ボタンクリックでサイドバー非表示
+    //×ボタンクリックイベント
     $closeBtn.on("click", function() {
-        $sidebar.removeClass("u-open");
-        $sidebarBg.removeClass("u-overlay");
+        $sidebar.removeClass("u-open");  //サイドバー非表示
+        $sidebarBg.removeClass("u-overlay"); //背景色透明度0 
     });
-});
 
+
+    
+/* pc画面時のサイドバー/背景の削除 */
+    //ブレイクポイントのメディアクエリを設定
+    const mediaQueryTablet = window.matchMedia("(min-width: 750px)");
+    const mediaQueryPc = window.matchMedia("(min-width: 1250px)");
+
+    //リスナーの設定
+    mediaQueryPc.addEventListener("change", handlePcChange);  //addEventListenerメソッドを使用、'change' イベントはメディアクエリの状態変更時に処理を実行することができる。（handlePcChange関数の実行）
+
+    //初回の状態チェック
+    handlePcChange(mediaQueryPc);
+
+    function handlePcChange(e) {
+        console.log("handlePcChange called", e.matches);  //handlePcChange関数の確認（ウィンドウ幅1250px以上でtrue、以下でfalse）
+        if (e.matches) {
+            $sidebarBg.removeClass("u-overlay");
+            $sidebar.removeClass("u-open");
+        }
+    }
+});
