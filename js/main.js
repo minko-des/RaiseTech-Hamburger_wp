3cf4acf876c273a60d1c1c6b07469b9d5820d4cd
minko-des <lijigongze@gmail.com>
2024-08-11 07:49:59 +0900

    UnderPage Coding 240811

--- js/main.js
+++ js/main.js
@@ -1,21 +1,7 @@
-/*
-document.addEventListener("DOMContentLoaded", function() {
-    const menuBtn = document.querySelector(".c-menu-btn");
-    const slidebar = document.querySelector(".l-sidebar");
-    const closeBtn = document.querySelector(".c-close-btn");
 
-    menuBtn.addEventListener("click", function() {
-        slidebar.classList.add("open");
-    });
-
-    closeBtn.addEventListener("click", function() {
-        slidebar.classList.remove("open");
-    });
-});
-*/
-
-/* サイドバー/背景の表示/非表示 */
+/* ボタンクリックでサイドバーと背景の表示/非表示 */
 $(document).ready(function() {
+
     const $menuBtn = $(".menu-btn");  //menuボタン
     const $sidebar = $(".p-sidebar");  //サイドバー
     const $closeBtn = $(".c-close-btn");  //×ボタン
@@ -25,17 +11,21 @@ $(document).ready(function() {
     $menuBtn.on("click", function() {
         $sidebar.addClass("u-open");  //サイドバースライドイン
         $sidebarBg.addClass("u-overlay"); //背景色透明度0.5
+        
+        toggleBodyScroll();  //ボタンクリック時に関数を呼び出す
     });
 
     //×ボタンクリックイベント
     $closeBtn.on("click", function() {
         $sidebar.removeClass("u-open");  //サイドバー非表示
         $sidebarBg.removeClass("u-overlay"); //背景色透明度0 
+        
+        toggleBodyScroll();  //ボタンクリック時に関数を呼び出す
     });
 
 
     
-/* pc画面時のサイドバー/背景の削除 */
+/* レスポンシブによるサイドバーと背景の削除（タブレット以下でサイドバー表示状態でpc画面幅に広げた場合）関数 */
     //ブレイクポイントのメディアクエリを設定
     const mediaQueryTablet = window.matchMedia("(min-width: 750px)");
     const mediaQueryPc = window.matchMedia("(min-width: 1250px)");
@@ -51,6 +41,35 @@ $(document).ready(function() {
         if (e.matches) {
             $sidebarBg.removeClass("u-overlay");
             $sidebar.removeClass("u-open");
+            $("body").removeClass("u-noScroll--body")
+            $(".l-sidebar").removeClass("u-noScroll--sidebar");
         }
     }
+
+
+
+/* サイドバー表示時の画面スクロール切替え関数 */
+    function toggleBodyScroll() {
+
+        if ($sidebar.hasClass("u-open")) {
+            $("body").addClass("u-noScroll--body");
+            $(".l-sidebar").addClass("u-noScroll--sidebar");
+        } else {
+            $("body").removeClass("u-noScroll--body");
+            $(".l-sidebar").removeClass("u-noScroll--sidebar");
+        }
+    }
+
+
+
+/* 写真の複製処理 */
+    const $photoSet = $(".photo-set");
+    const imageUrl = "./images/single-content.jpg"; //画像のURL
+    const numberOfImages = "8"; //画像の数
+
+    for (let i = 0; i < numberOfImages; i++) { //ループ処理
+    const $img = $("<img>").attr("src", imageUrl).attr("alt", "写真一覧"); //新しいimg要素を生成し画像のパス、alt属性を設定、変数$imgに格納
+    $photoSet.append($img); //$photoSet要素に$img要素を追加
+    }
+
 });
