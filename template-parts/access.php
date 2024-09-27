2d1236259a86a2c906dab780eb4ba0241b5ccfb7
minko-des <lijigongze@gmail.com>
2024-09-27 16:43:05 +0900

    デプロイデータ

--- template-parts/access.php
+++ template-parts/access.php
@@ -1,5 +1,10 @@
-<article class="p-access">
-    <img class="access__img" src="<?php echo esc_url( get_theme_file_uri('/images/map.jpg.webp') ); ?>" alt="マップ画像">
+<article class="p-access">   
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.7477988371998!2d139.7428633386239!3d35.658584831334494!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bbd9009ec09%3A0x481a93f0d2a409dd!2z5p2x5Lqs44K_44Ov44O8!5e0!3m2!1sja!2sjp!4v1727416477683!5m2!1sja!2sjp" 
+        style="border:0;" 
+        allowfullscreen="" 
+        loading="lazy" 
+        referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
     <div class="overlay--all"></div>
     <div class="overlay--half"></div>
     <div class="access__text">
